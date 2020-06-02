ec3e4eb3669065a16c78fa110cff088a1b057d6a
Ulisse Mini <ulisse.mini@gmail.com>
2020-06-02 14:12:22 -0400

    Move stock visualization to separate page

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,22 @@
+import Link from "next/link";
 import Head from "next/head";
-import data from "../lib/data"
-
-function BarChart({ data }) {
-  const chartHeight = 400
-  const chartWidth = 600
-  const boxWidth = 10
-
-  const high = Math.max(...data)
-  const heightScalar = chartHeight / high
-  const widthScalar = chartWidth / (data.length * boxWidth)
-  console.log('width scaler:', widthScalar)
-  console.log('height scaler:', heightScalar)
-
-  let elems = [];
-  for (let i in data) {
-    const gt = data[i - 1] ? data[i - 1] < data[i] : false;
-    const style = {
-      width: `${boxWidth * widthScalar}px`,
-      height: `${data[i] * heightScalar}px`,
-      background: gt ? "#0f0" : "#f00",
-    };
-    elems.push(<div className="bar" style={style} key={i} />);
-  }
-
-  return <div
-    className="chart"
-    children={elems}
-    style={{
-      height: `${chartHeight}px`,
-      width: `${chartWidth}px`,
-      background: '#222',
-    }}
-  />;
-}
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
-        <title>Stonks</title>
+        <title>Data</title>
       </Head>
 
       <main>
-        <h2>Behold, the stonks</h2>
-        <BarChart data={data} />
-        <p>High: {Math.max(...data)}, low: {Math.min(...data)}</p>
+        <h2>Hi! Welcome to uli's data playground</h2>
+
+        <nav>
+          <Link href="/stocks">
+            <a>Visualize some stocks</a>
+          </Link>
+        </nav>
       </main>
-    </div>
+    </>
   );
 }
---
+++ pages/stocks.js
@@ -0,0 +1,56 @@
+import Head from "next/head";
+import data from "../lib/data"
+import Link from "next/link";
+
+function BarChart({ data }) {
+  const chartHeight = 400
+  const chartWidth = 600
+  const boxWidth = 10
+
+  const high = Math.max(...data)
+  const heightScalar = chartHeight / high
+  const widthScalar = chartWidth / (data.length * boxWidth)
+  console.log('width scaler:', widthScalar)
+  console.log('height scaler:', heightScalar)
+
+  let elems = [];
+  for (let i in data) {
+    const gt = data[i - 1] ? data[i - 1] < data[i] : false;
+    const style = {
+      width: `${boxWidth * widthScalar}px`,
+      height: `${data[i] * heightScalar}px`,
+      background: gt ? "#0f0" : "#f00",
+    };
+    elems.push(<div className="bar" style={style} key={i} />);
+  }
+
+  return <div
+    className="chart"
+    children={elems}
+    style={{
+      height: `${chartHeight}px`,
+      width: `${chartWidth}px`,
+      background: '#222',
+    }}
+  />;
+}
+
+export default function Stocks() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Stonks</title>
+      </Head>
+
+      <Link href="/">
+        <a className="back">{"<- back"}</a>
+      </Link>
+
+      <main>
+        <h2>Behold, the stonks</h2>
+        <BarChart data={data} />
+        <p>High: {Math.max(...data)}, low: {Math.min(...data)}</p>
+      </main>
+    </div>
+  );
+}
